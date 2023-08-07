c00b60f5b29b463f9358f1c0b3e2d9d558e7f881
Kumar Shashank <kumar.shashank@weverve.com>
2023-08-07 16:41:04 +0530

    header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <>
-                <h1>hello</h1>
+            <div className="flex items-center justify-between px-20 py-2 drop-shadow-md bg-gray-100">
+                <img className="w-8" src="https://seeklogo.com/images/S/swiggy-logo-8EF8260FA4-seeklogo.com.png" alt="" />
+                {
+                    isLoggedIn
+                        ? <button className=" px-2 py-1 bg-gray-400 rounded" onClick={() => setIsLoggedIn(!isLoggedIn)}>Log In  </button>
+                        : <button className=" px-2 py-1 bg-gray-400 rounded" onClick={() => setIsLoggedIn(!isLoggedIn)}>Log Out </button>
+                }
+            </div>
         </>
     )
 }
