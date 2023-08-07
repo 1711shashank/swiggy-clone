f0ef89378d5fd3c191dd6aa8185dc2bd2c9512c5
Kumar Shashank <kumar.shashank@weverve.com>
2023-08-07 15:09:01 +0530

    Applayout

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './src/Header';
 
-const heading1 = React.createElement(
-    "h1",
-    { id: "head1" },
-    "This is a header1"
-);
-const heading2 = React.createElement(
-    "h2",
-    { id: "head2" },
-    "This is a header2"
-);
-const container = React.createElement(
-    "div",
-    { id: "container" },
-    [heading1, heading2]
-);
 
-const HeaderComponent = () => {
+
+const AppLayout = () => {
     return (
         <>
-        <h1> Header from HeaderComponent</h1>
+           <Header/>
         </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-root.render(<HeaderComponent/>);
+root.render(<AppLayout />);
